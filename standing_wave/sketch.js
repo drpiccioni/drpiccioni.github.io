f249b1c94754157194bb4121731edb023a4f4e9e
Richard Piccioni <rpiccioni@marin.edu>
2020-12-16 17:05:10 -0800

    Update sketch.js
    color to green

--- standing_wave/sketch.js
+++ standing_wave/sketch.js
@@ -46,8 +46,8 @@ function draw() {
   translate(0,height/2);
 
   calcWaveA(1);
-  renderLine(color(250,0,0),1);
- // renderLine(color(0,250,0),1);
+  //renderLine(color(250,0,0),1);
+  renderLine(color(0,250,0),1);
 
   calcWaveB(-1);
   renderLine(color(0,0,250),1);
