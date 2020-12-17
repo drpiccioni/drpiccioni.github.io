b1df1e23b4c041cb2fd7d0cc5d08f2cb316cbacc
Richard Piccioni <rpiccioni@marin.edu>
2020-12-16 17:31:41 -0800

    Update sketch.js
    decrease canvas width

--- standing_wave/sketch.js
+++ standing_wave/sketch.js
@@ -1,22 +1,17 @@
 
-//var w;                // Width of entire wave
 var amplitude = 50.0; // Height of wave
-//var wavelength = 50;   // How many pixels before the wave repeats
 var v = 12; // fixed wave speed
 var dx;               // Value for incrementing x
 var yvalues;  // Using an array to store height values for the wave
-//var k = 2*Math.PI/wavelength;
 var omega = Math.PI;
 var phi   = Math.PI;
 var t = 0;
 var dt = 0.1;
 
 function setup() {
-
   frameRate(30);
-  canvas = createCanvas(windowWidth, 0.9*windowHeight);
+  canvas = createCanvas(2*windowWidth/3, 0.9*windowHeight);
   canvas.parent('sketch-holder');
- // w = 0.5*width;
 
   y = new Array(200);
 
@@ -35,19 +30,18 @@ function setup() {
   phiSliderLabel = createP('&phi;'+ ' / '+ '&pi; = '+phiSlider.value());
   phiSliderLabel.parent('sketch-holder');
   phiSliderLabel.position(200,phiSlider.y+10);
-
 }
 
 function draw() {
   background(255);
   speedSliderLabel.html('&omega;'+ ' / '+ '&pi; = '+speedSlider.value());
   phiSliderLabel.html('&phi;'+ ' / '+ '&pi; = '+phiSlider.value());
-  //t = millis()/1000;
+  
   translate(0,height/2);
 
   calcWaveA(1);
-  //renderLine(color(250,0,0),1);
-  renderLine(color(0,250,0),1);
+  renderLine(color(250,0,0),1);
+  //renderLine(color(0,250,0),1);
 
   calcWaveB(-1);
   renderLine(color(0,0,250),1);
@@ -55,22 +49,20 @@ function draw() {
   calcSum(1);
   renderLine(color(0,0,0),2);
 
-
-
-
   push();
+  
   strokeWeight(1);
   stroke(0);
   line(0,height/2,width,height/2);
+  
   pop();
-  //dt = speedSlider.value();
+  
   omega = speedSlider.value()*Math.PI;
   phi   = phiSlider.value()*Math.PI
   t = t+dt;
 }
 
 function calcWaveA(direction) {
-
   x = 0;
   for (var x = 0; x < y.length; x += 1) {
     y[x] =  Math.sin(omega*(t - direction*x/v))*amplitude;
@@ -78,7 +70,6 @@ function calcWaveA(direction) {
 }
 
 function calcWaveB(direction) {
-
   x = 0;
   for (var x = 0; x < y.length; x += 1) {
     y[x] =  Math.sin(omega*(t - direction*x/v)+phi)*amplitude;
@@ -86,20 +77,14 @@ function calcWaveB(direction) {
 }
 
 function calcSum(direction) {
-
   x = 0;
   for (var x = 0; x < y.length; x += 1) {
       y[x] =  Math.sin(omega*(t - direction*x/v))*amplitude + Math.sin(omega*(t + direction*x/v)+phi)*amplitude;
   }
-
 }
-
-
-
 
 function renderLine(color_,weight_) {
   //this function puts a line through all the positions defined above.
-
   push();
   noFill();
   stroke(color_);
