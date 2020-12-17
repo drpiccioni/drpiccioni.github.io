a8b0893b38bd94ca20b31d9d6ede3e7fcbda7226
Richard Piccioni <rpiccioni@marin.edu>
2020-12-17 14:11:39 -0800

    Update sketch.js
    replace with sketch developed in p5 ide, plus sketch-holder statements

--- standing_wave/sketch.js
+++ standing_wave/sketch.js
@@ -1,87 +1,87 @@
-
 var amplitude = 50.0; // Height of wave
-var v = 10; // fixed wave speed
-var dx;               // Value for incrementing x
-var yvalues;  // Using an array to store height values for the wave
-var omega = Math.PI;
-var phi   = Math.PI;
+var omega = 0;
+var phi   = 0;
 var t = 0;
 var dt = 0.1;
+var v = 5.92         // fine-tuning by the Almighty
+var tScale = 0.3    // about right on my system
 
 function setup() {
+  
   frameRate(30);
-  canvas = createCanvas(windowWidth, 0.9*windowHeight);
+  canvas = createCanvas(0.7*windowWidth, 0.9*windowHeight);
   canvas.parent('sketch-holder');
-
   y = new Array(200);
-
-  speedSlider = createSlider(0.0,1.0,0.0,0.1);
+    
+  omegaSlider = createSlider(0.1,1,0.5,0.1);
   speedSlider.parent('sketch-holder');
-  speedSlider.position(20,20);
-  speedSlider.class("sim-slider gray");
-  speedSliderLabel = createP('&omega;'+ ' / '+ '&pi; = '+speedSlider.value());
-  speedSliderLabel.parent('sketch-holder');
-  speedSliderLabel.position(20,speedSlider.y+10);
-
-  phiSlider = createSlider(0.0,1.0,0.0,1.0);
+  omegaSlider.position(20,20);
+  omegaSlider.class("sim-slider gray");
+  omegaSliderLabel = createP()
+  omegaSliderLabel.position(20,omegaSlider.y+10);
+ 
+  phiSlider = createSlider(0,1,1,1);
   phiSlider.parent('sketch-holder');
   phiSlider.position(200,20);
   phiSlider.class("sim-slider gray");
-  phiSliderLabel = createP('&phi;'+ ' / '+ '&pi; = '+phiSlider.value());
-  phiSliderLabel.parent('sketch-holder');
+  phiSliderLabel = createP()
   phiSliderLabel.position(200,phiSlider.y+10);
+
+  compSlider = createSlider(0,1,1,1);
+//  compSlider.parent('sketch-holder');
+  compSlider.position(400,20);
+  compSlider.class("sim-slider gray");
+  compSliderLabel = createP();
+  compSliderLabel.position(400,compSlider.y+10);
+
 }
 
 function draw() {
-  background(255);
-  speedSliderLabel.html('&omega;'+ ' / '+ '&pi; = '+speedSlider.value());
-  phiSliderLabel.html('&phi;'+ ' / '+ '&pi; = '+phiSlider.value());
   
-  translate(0,height/2);
+  background(255);
+  
+  omegaSliderLabel.html('&omega;'+' / '+'&pi; = '+omegaSlider.value());
+  
+  phiSliderLabel.html('&phi;'+ ' / '+'&pi; = '+phiSlider.value());
+  
+  compSliderLabel.html('components');
+  
+  omega = tScale*omegaSlider.value()*Math.PI;
 
-  calcWaveA(1);
-  renderLine(color(250,0,0),1);
-  //renderLine(color(0,250,0),1);
+  phi   = phiSlider.value()*Math.PI
 
-  calcWaveB(-1);
-  renderLine(color(0,0,250),1);
+  showcomp = compSlider.value()
 
-  calcSum(1);
-  renderLine(color(0,0,0),2);
+  translate(7,height/2);  // more fine-tuning
+  
+  if(showcomp){
+    calcWave(1,amplitude,0,omega,v,phi,t);
+    renderLine(color(250,0,0),1);
+
+    calcWave(-1,0,amplitude,omega,v,phi,t);
+    renderLine(color(0,0,250),1);
+
+  }
+  
+  calcWave(1,amplitude,amplitude,omega,v,phi,t);
+  renderLine(color(0,0,0),1);
 
   push();
-  
   strokeWeight(1);
   stroke(0);
-  line(0,height/2,width,height/2);
-  
+  line(0,0,width,0);
   pop();
-  
-  omega = speedSlider.value()*Math.PI;
-  phi   = phiSlider.value()*Math.PI
   t = t+dt;
 }
 
-function calcWaveA(direction) {
+function calcWave(direction_,ampA_,ampB_,omega_,v_,phi_,t_) {
   x = 0;
   for (var x = 0; x < y.length; x += 1) {
-    y[x] =  Math.sin(omega*(t - direction*x/v))*amplitude;
+    y[x] =  ampA_*Math.sin(omega_*(t_ - direction_*x/v_)) +      
+            ampB_*Math.sin(omega_*(t_ + direction_*x/v_)+phi_);
   }
 }
 
-function calcWaveB(direction) {
-  x = 0;
-  for (var x = 0; x < y.length; x += 1) {
-    y[x] =  Math.sin(omega*(t - direction*x/v)+phi)*amplitude;
-  }
-}
-
-function calcSum(direction) {
-  x = 0;
-  for (var x = 0; x < y.length; x += 1) {
-      y[x] =  Math.sin(omega*(t - direction*x/v))*amplitude + Math.sin(omega*(t + direction*x/v)+phi)*amplitude;
-  }
-}
 
 function renderLine(color_,weight_) {
   //this function puts a line through all the positions defined above.
@@ -90,7 +90,7 @@ function renderLine(color_,weight_) {
   stroke(color_);
   strokeWeight(weight_);
   beginShape();
-  for (var x = 0; x < y.length; x += 3) {
+  for (var x = -1; x < y.length; x += 1) {
     curveVertex(map(x,0,y.length,0,width), -y[x]);
   }
   endShape();
